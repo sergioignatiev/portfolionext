3672a1e0927331f20b38a2398ccc90c0e803dc38
SergioIgnatev <ignatevsergey17@gmail.com>
2023-12-16 18:06:13 +0300

    h

--- components/main/mainbg.tsx
+++ components/main/mainbg.tsx
@@ -2,7 +2,7 @@
 import bg from './mainbg.module.css'
 
 export function MainBg(){
-    return  <div className={`${bg.grid} min-w-full md:grid  grid-cols-2  text-white -z-50 min-h-[140vh]    top-0 bg-[#222629]`}>
+    return  <div className={`${bg.grid} min-w-full md:grid  grid-cols-2  text-white -z-50 min-h-[140vh]     top-0 bg-[#222629]`}>
     <div className=" h-0 w-0"></div>
     <div className={`${bg.mainimg} min-w-full`}></div>
     </div>
