41933381c2d3054ef0f72af95e6667f7426232fa
SergioIgnatev <ignatevsergey17@gmail.com>
2023-12-16 18:11:09 +0300

    l

--- components/main/mainpage.tsx
+++ components/main/mainpage.tsx
@@ -3,8 +3,8 @@ export function Mainpage(){
 return <div className="absolute text-white 2xl:mt-[259px] mt-[170px] 
 md:border-l-[1px] border-[white] md:ml-[68px] md:pl-[91px] ml-[20px] mr-[20px]">
 <h3 className="2xl:text-[42px] text-[30px] md:font-bold font-semibold">Фронтенд разработчик</h3>
-<h1 className="2xl:text-[130px] md:text-[80px] text-[60px] font-semibold">Сергей Игнатьев</h1>
-<div className="2xl:max-w-[552px] max-w-[400px] 2xl:text-[20px] text-[15px] font-normal 2xl:mt-[54px] mt-[30px]">
+<h1 className="2xl:text-[130px] md:text-[80px] text-[60px] font-semibold md:mt-0 mt-[70px]">Сергей Игнатьев</h1>
+<div className="2xl:max-w-[552px] max-w-[400px] 2xl:text-[20px] text-[17px] font-normal 2xl:mt-[54px] mt-[30px]">
 <p>Здравствуйте! Я Сергей из Новороссийска. </p>
 <p className="2xl:mt-[20px] mt-[15px]">
 Моя душа лежит к кодингу, больше всего я люблю верстать сайты. Активно развиваюсь как веб-разработчик.</p>
