196161d04205b91175dfebb0f9226ec6454151d9
SergioIgnatev <ignatevsergey17@gmail.com>
2023-12-24 22:50:16 +0300

    minor

--- app/projects/components/units/secondSwUnit.tsx
+++ app/projects/components/units/secondSwUnit.tsx
@@ -3,10 +3,10 @@
 const SecondSwUnit = ({id,name}:{id:string,name:string}) => {
   return (
     <div className=" h-[117px] flex justify-center items-center ">
-        <div className="text-[96px] text-white font-extrabold opacity-10 absolute left-[49px] top-[-20px]">{id}</div>
+        <div className="2xl:text-[96px] h-full flex items-center  text-[60px] text-white font-extrabold opacity-10 absolute left-[49px] ">{id}</div>
         <div className="text-white border-x-[1px]
         h-[80px]
-        border-white w-full flex justify-center items-center  text-[24px] font-bold">{name}</div>
+        border-white w-full flex justify-center items-center  2xl:text-[24px] text-[17px] font-bold">{name}</div>
     </div>
   )
 }
