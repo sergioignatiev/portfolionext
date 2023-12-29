d80461d2ccde75cbd356f98b266b85cd28700243
SergioIgnatev <ignatevsergey17@gmail.com>
2023-12-29 21:46:24 +0300

    h

--- components/nav.tsx
+++ components/nav.tsx
@@ -7,11 +7,11 @@ import { usePathname } from 'next/navigation'
 let arrow=<svg xmlns="http://www.w3.org/2000/svg" width="9" height="13" viewBox="0 0 9 13" fill="none">
 <path fill-rule="evenodd" clip-rule="evenodd" d="M2 0.0138483L7.72899 0L8.5 5.5L6.9291 7.22039L6.28481 2.82077L1.22695 8.49332L0 7.37298L5.07255 1.68406L0.430738 1.73245L2 0.0138483Z" fill="#E9E9E9"/>
 </svg>
-function NavUnit({children,brdr,brdrr}:{children:React.ReactNode|string,brdr:boolean,brdrr:boolean}){
+function NavUnit({children,brdr,brdrr,vis}:{children:React.ReactNode|string,brdr:boolean,brdrr:boolean,vis:boolean}){
 
-return <div style={{borderRight:brdrr?'2px solid white':""}} className={classnames({
-    '':brdr===false,
-    'border-b-[1px] ':brdr===true,
+return <div style={{borderRight:brdrr?'2px solid white':"",borderBottom:brdr?"1px solid white":""}} className={classnames({
+    '':vis===true,
+    'lg:flex hidden':vis!==true,
 '2xl:text-[16px] text-[12px] font-bold flex \
  justify-center\
   items-center 2xl:p-[20px] p-[10px] border-b-white border-r-white  w-[100%]':true
@@ -33,7 +33,7 @@ export function Nav(){
  
 
  <nav className="flex justify-between w-full ">
-<NavUnit brdr={false} brdrr={false} >
+<NavUnit brdr={false} brdrr={false} vis={false}>
 <Image
     src='/mouse.png'
  alt='mouse'
@@ -44,26 +44,26 @@ export function Nav(){
 </NavUnit>
 
  
- <NavUnit brdr={true} brdrr={true}>
+ <NavUnit brdr={true} brdrr={true} vis={true}>
     <Link className="flex items-center" href='/'>Сергей Игнатьев {arrow}</Link>
  </NavUnit>
- <NavUnit brdr={true} brdrr={true}>
+ <NavUnit brdr={true} brdrr={true} vis={true}>
     <Link className="flex items-center" href='/projects'>Проекты {arrow}</Link>
  </NavUnit>
- <NavUnit brdr={true} brdrr={true}>
+ <NavUnit brdr={true} brdrr={true} vis={true}>
     <Link className="flex items-center" href='/skills'>Навыки {arrow}</Link>
  </NavUnit>
- <NavUnit brdr={true} brdrr={true}>
+ <NavUnit brdr={true} brdrr={true} vis={false}>
     <Link className="flex items-center" href='/skills'>Контакты  {arrow}</Link>
  </NavUnit>
- <NavUnit brdr={true} brdrr={false}>
+ <NavUnit brdr={true} brdrr={false} vis={false}>
  +7 (965) 459 35-51
    
  </NavUnit>
- <NavUnit brdr={true} brdrr={true}>
+ <NavUnit brdr={true} brdrr={true} vis={false}>
  ignatevsergey17@gmail.com
  </NavUnit>
- <NavUnit brdr={true} brdrr={false}>
+ <NavUnit brdr={true} brdrr={false} vis={false}>
     <Link className="flex items-center" href='/skills'>RU{arrow}</Link>
  </NavUnit>
  </nav>
