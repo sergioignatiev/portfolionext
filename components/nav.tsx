9703f5c1f3948a6f8f5ecef4f7b79d610e5a2495
SergioIgnatev <ignatevsergey17@gmail.com>
2023-12-31 18:02:27 +0300

    refact

--- components/nav.tsx
+++ components/nav.tsx
@@ -9,12 +9,12 @@ let arrow=<svg xmlns="http://www.w3.org/2000/svg" width="9" height="13" viewBox=
 </svg>
 function NavUnit({children,brdr,brdrr,vis}:{children:React.ReactNode|string,brdr:boolean,brdrr:boolean,vis:boolean}){
 
-return <div style={{borderRight:brdrr?'2px solid white':"",borderBottom:brdr?"1px solid white":""}} className={classnames({
-    '':vis===true,
+return <div style={{borderRight:brdrr&&window.innerWidth>700?'1px solid white':"",borderBottom:brdr&&window.innerWidth>700?"1px solid white":""}} className={classnames({
+    "":vis===true,
     'lg:flex hidden':vis!==true,
 '2xl:text-[16px] text-[12px] font-bold flex \
- justify-center\
-  items-center 2xl:p-[20px] p-[10px] border-b-white border-r-white  w-[100%]':true
+ justify-center \
+  items-center  2xl:p-[20px] p-[10px] lg:border-b-white lg:border-r-white  w-[100%]':true
   })}
    
    >{children}</div>
