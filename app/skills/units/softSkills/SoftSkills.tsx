de73bea59d99252ae569c88e2e6f260d29a1a22b
SergioIgnatev <ignatevsergey17@gmail.com>
2024-01-05 18:33:50 +0300

    w

--- app/skills/units/softSkills/SoftSkills.tsx
+++ app/skills/units/softSkills/SoftSkills.tsx
@@ -3,7 +3,7 @@ import { SoftSkillsUnit } from './softSkillsUnit'
 import { Percentage } from '../percentage'
 const SoftSkills = () => {
   return (
-    <div className='xl:pl-[117px] w-full  px-[20px] xl:w-[50%] 2xl:pt-[162px]  lg:pt-[120px]  xl:pr-[95px]'>
+    <div className='xl:pl-[117px] w-full 2xl:ml-[36px]  px-[20px] xl:w-[50%] 2xl:pt-[162px]  lg:pt-[120px]  xl:pr-[95px]'>
 <div className="w-full text-[24px] font-extrabold border-b-[white] border-b-[1px] py-[3px]">Soft Skills</div>
 <div className="mt-[100px] min-h-[50vh] flex flex-col  justify-between ">
     <SoftSkillsUnit softskill='English Language' perc={88}/>
