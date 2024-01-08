295d5fb3d0635ce44692843eae5f9fdf3215ce4b
SergioIgnatev <ignatevsergey17@gmail.com>
2024-01-08 23:14:57 +0300

    stars

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
 import { MainBg } from "@/components/main/mainbg"
 import { Mainpage } from "@/components/main/mainpage"
-
+import { stars,starsleft } from "./svgs"
 export default function Home() {
+  
   return (
   <div className='relative'>
-
+    <div className="absolute right-0 2xl:visible invisible">{starsleft}</div>
+<div className="absolute bottom-0 2xl:visible invisible">{stars}</div>
 <Mainpage/>
 <MainBg/>
+
   </div>
   )
 }
