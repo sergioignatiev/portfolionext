05ae2f2129321c6e4593168a85eb1f13be64a264
SergioIgnatev <ignatevsergey17@gmail.com>
2024-01-08 22:17:03 +0300

    footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,33 +1,42 @@
-import React from 'react'
+
+import Image from 'next/image'
+function FooterUnit({children,w}:{children:React.ReactNode,w:string}){
+return <div className={`flex items-center justify-center border-collapse
+border-l-[1px] border-r-[0.5px] border-white ${w} h-[96px] text-[16px]
+`}>{children}</div>
+}
 
 const Footer = () => {
+  const logo=<svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 26 26" fill="none">
+  <circle cx="13" cy="13" r="12.5" stroke="#E9E9E9"/>
+</svg>
+const github=<svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 26 26" fill="none">
+<circle cx="13" cy="13" r="12.5" stroke="#E9E9E9"/>
+<path d="M15.4845 21.1231V18.1658C15.5824 17.2397 15.3297 16.3103 14.7809 15.5783C16.8919 15.5783 19.0029 14.0996 19.0029 11.5121C19.0592 10.5879 18.8129 9.67858 18.2992 8.92449C18.4962 8.07429 18.4962 7.18712 18.2992 6.33691C18.2992 6.33691 17.5955 6.33691 16.1882 7.44587C14.3305 7.07622 12.4166 7.07622 10.5589 7.44587C9.15157 6.33691 8.44791 6.33691 8.44791 6.33691C8.23681 7.18712 8.23681 8.07429 8.44791 8.92449C7.93555 9.67554 7.68687 10.59 7.74424 11.5121C7.74424 14.0996 9.85523 15.5783 11.9662 15.5783C11.6918 15.9405 11.4877 16.3545 11.3681 16.7981C11.2485 17.2417 11.2133 17.7075 11.2626 18.1658M11.2626 18.1658V21.1231M11.2626 18.1658C8.08904 19.6444 7.74424 16.6872 6.33691 16.6872" stroke="#E9E9E9" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
   return (
-    <footer className="footer p-10 bg-base-200 text-base-content">
-    <aside>
-      <svg width="50" height="50" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd" className="fill-current"><path d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path></svg>
-      <p>ACME Industries Ltd.<br/>Providing reliable tech since 1992</p>
-    </aside> 
-    <nav>
-      <header className="footer-title">Services</header> 
-      <a className="link link-hover">Branding</a>
-      <a className="link link-hover">Design</a>
-      <a className="link link-hover">Marketing</a>
-      <a className="link link-hover">Advertisement</a>
-    </nav> 
-    <nav>
-      <header className="footer-title">Company</header> 
-      <a className="link link-hover">About us</a>
-      <a className="link link-hover">Contact</a>
-      <a className="link link-hover">Jobs</a>
-      <a className="link link-hover">Press kit</a>
-    </nav> 
-    <nav>
-      <header className="footer-title">Legal</header> 
-      <a className="link link-hover">Terms of use</a>
-      <a className="link link-hover">Privacy policy</a>
-      <a className="link link-hover">Cookie policy</a>
-    </nav>
+<>
+    <div className="h-[7px] bg-[#1f1f1f]"></div>
+    <footer className="bg-[#1f1f1f] pb-[46px] text-[#e9e9e9] px-[56px]  border-t-[1px] border-white  w-[100%]">
+   
+   <div className="flex justify-between">
+<FooterUnit w='w-1/6'>
+<div className="flex mx-[46px] justify-between w-full">
+  <div className="">{logo}</div>
+  <div className="">2024</div>
+  <div className="">Анна Севоян</div>
+</div>
+
+</FooterUnit>
+<FooterUnit w='w-3/6'><div className="flex mx-[100px]">
+  <div className="">
+   <div className="">{github}</div>
+  </div>
+  </div></FooterUnit>
+<FooterUnit w='w-2/6'><h1>FOOTER UNIT</h1></FooterUnit>
+   </div>
   </footer>
+  </>
   )
 }
 
