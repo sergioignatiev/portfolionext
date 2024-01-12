705e77844cc67f2f7e0006a6a0fb7b7b6173efe3
SergioIgnatev <ignatevsergey17@gmail.com>
2024-01-12 10:45:43 +0300

    back

--- components/main/mainmobile/mainmobile.tsx
+++ components/main/mainmobile/mainmobile.tsx
@@ -3,9 +3,9 @@ import Image from 'next/image'
 const Mainmobile = () => {
   return (
     <div className=" bg-[#1f1f1f] text-[#e9e9e9] pt-[120px]   mt-0">
-<div className="pl-[26px] pb-[16px]">
+<div className="pl-[20px] pb-[16px]">
 <h2 className=' text-[16px]'>Фронтенд разработчик</h2>
-<h1 className='text-[36px] font-bold'>Игнатьев Сергей</h1>
+<h1 className='text-[36px] font-bold'>Сергей Игнатьев</h1>
 </div>
 <img src="/photo.png" alt="" />
     </div>
