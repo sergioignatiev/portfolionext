1124d20b29a99237edbd18879dd4418596edd5f9
SergioIgnatev <ignatevsergey17@gmail.com>
2024-01-12 11:01:03 +0300

    t

--- components/main/mainmobile/mainmobile.tsx
+++ components/main/mainmobile/mainmobile.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import Image from 'next/image'
+import { T } from '../text'
+import { texts } from '../text'
 const Mainmobile = () => {
   return (
     <div className=" bg-[#1f1f1f] text-[#e9e9e9] pt-[120px]   mt-0">
@@ -8,7 +9,11 @@ const Mainmobile = () => {
 <h1 className='text-[36px] font-bold'>Сергей Игнатьев</h1>
 </div>
 <img src="/photo.png" alt="" />
+
+<div className="pl-[36px] pr-[18px]">
+{texts.map(t=><div className='text-[12px] mt-[20px] text-[#e9e9e9]' key={t.id}>{t.text}</div>)}</div>
     </div>
+    
   )
 }
 
--- components/main/mainpage.tsx
+++ components/main/mainpage.tsx
@@ -1,4 +1,6 @@
 import Link from "next/link"
+import { T } from "./text"
+import { texts } from "./text"
 export let button:string='2xl:w-[280px] md:w-[170px] md:h-[35px] \
 w-[150px] h-[30px] 2xl:h-[56px] 2xl:rounded-[16px] rounded-[10px] 2xl:text-[20px]  duration-1000 hover:scale-[90%]'
 function Paragraph({children}:{children:string}){
@@ -17,18 +19,7 @@ return <div className="absolute text-white 2xl:mt-[259px] xl:mt-[170px] mt-[150p
 
  ">
 
-<Paragraph>
-    Здравствуйте! Я Сергей из Новороссийска. 
-    </Paragraph>
-
-
-<Paragraph>
-Люблю Веб-разработку и активно развиваюсь в данном направлении.
-</Paragraph>
-<Paragraph>
-    Я работал со стартапами,на фрилансе  и учился самостоятельно на курсах — w3schools и freeCodeCamp.
-</Paragraph>
-
+<div className="">{texts.map(t=><Paragraph key={t.id}>{t.text}</Paragraph>)}</div>
 
 </div>
 <div className="flex 2xl:mt-[83px] mt-[50px] 2xl:mb-[214px] mb-[110px]">
